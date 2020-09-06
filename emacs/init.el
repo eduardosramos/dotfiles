997601cd67ce7cadadbf1650531bc8e1641768d1
Suporte Villa <suporte@villafacil.com.br>
2020-09-06 19:49:30 -0300

    Init atualizado

--- emacs/init.el
+++ emacs/init.el
@@ -1,58 +1,121 @@
-;; Remover boas-vindas
 (setq inhibit-startup-message t)
 
-;; Remover menus
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-
-;; Remover barra de rolagem
-(scroll-bar-mode -1)
-
-;; Numeros nas linhas
 (global-linum-mode t)
 
-;; Tamanho da fonte
 (set-face-attribute  'default nil :height 120)
 
-;; Gerenciamento de pacotes
-(require 'package)
-(setq package-enable-at-startup nil) ; desabilitar inicio de ativacao
+(set-language-environment "UTF-8")
 
-;; Melpa - repositório
+(set-default-coding-systems 'utf-8-unix)
+
+";;; Commentary:"
+(require 'package)
+(setq package-enable-at-startup nil)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-(package-initialize) ; iniciar pacotes
+(package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (use-package try
-  :ensure t); C-x C-e executa código Lisp e baixa o pacote.
+  :ensure t)
 
 (use-package which-key
   :ensure t
-  :config (which-key-mode)) ; Achando o comando certo para qqr coisa no emacs
-(which-key-setup-side-window-right)
+  :config (which-key-mode))
 
-; Melpa Stuff
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)))
+
+(use-package all-the-icons
+  :ensure t)
+  
+(use-package neotree
+  :ensure t
+  :config
+  (progn
+    (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+  :bind (("C-\\" . neotree-toggle)))
+
+(use-package ace-window
+  :ensure t
+  :bind (("C-x o" . ace-window)))
+
+ ;; Multiplos cursores
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
+
+;;; Meus atalhos
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "M-<down>") 'enlarge-window)
+(global-set-key (kbd "M-<up>") 'shrink-window)
+(global-set-key (kbd "M-<left>") 'enlarge-window-horizontally)
+(global-set-key (kbd "M-<right>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-z") 'undo)
+
+;; (global-set-key (kbd "C-s") 'save-buffer)
+;; Verificando syntax
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode t))
+
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key try use-package)))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#2d2d2d" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#cccccc"))
+ '(beacon-color "#f2777a")
+ '(csv-separators (quote (";" "	")))
+ '(custom-enabled-themes (quote (rebecca)))
+ '(custom-safe-themes
+   (quote
+    ("e208e45345b91e391fa66ce028e2b30a6aa82a37da8aa988c3f3c011a15baa22" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(fci-rule-color "#515151")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(frame-background-mode (quote dark))
+ '(package-selected-packages
+   (quote
+    (live-py-mode rebecca-theme pdf-view-restore pdfgrep pdf-tools csv-mode ergoemacs-mode neotree auto-complete which-key try use-package)))
+ '(send-mail-function (quote mailclient-send-it))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f2777a")
+     (40 . "#f99157")
+     (60 . "#ffcc66")
+     (80 . "#99cc99")
+     (100 . "#66cccc")
+     (120 . "#6699cc")
+     (140 . "#cc99cc")
+     (160 . "#f2777a")
+     (180 . "#f99157")
+     (200 . "#ffcc66")
+     (220 . "#99cc99")
+     (240 . "#66cccc")
+     (260 . "#6699cc")
+     (280 . "#cc99cc")
+     (300 . "#f2777a")
+     (320 . "#f99157")
+     (340 . "#ffcc66")
+     (360 . "#99cc99"))))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
-
-
-
-
